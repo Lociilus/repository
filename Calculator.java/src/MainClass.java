f53da43e9946d1b330260061417c2bce5db313ea
LTona <LTona@192.168.1.77>
2021-10-13 15:17:12 -0400

    Calc

---
+++ Calculator.java/src/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass
+{
+	public static void main(String args[])
+	{
+		 Menu myMenu = new Menu();
+		 
+		 myMenu.start();
+		 
+		 System.out.println("\nProgram terminates.");
+		 System.exit(0);
+	}
+}
