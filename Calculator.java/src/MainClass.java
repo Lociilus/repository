f5bfc83179fb7ddafdd1bd4b2e26022de01368ce
Lociilus <92417234+Lociilus@users.noreply.github.com>
2021-10-14 08:00:59 -0400

    Update MainClass.java

--- Calculator.java/src/MainClass.java
+++ Calculator.java/src/MainClass.java
@@ -4,7 +4,26 @@ public class MainClass
 	{
 		 Menu myMenu = new Menu();
 		 
-		 myMenu.start();
+		 int menuSelection = myMenu.showMenu();
+		
+		if (menuSelection == 1) {
+			//do something 
+		}
+		else if (menu selection == 2) {
+			//do something 
+		}
+		else if (menu selection == 3) {
+			//do something 
+		}
+		else if (menu selection == 4) {
+			//do somehting 
+		}
+		else if (menu selection == 5) {
+			//do something 
+		}
+		else if (menu selection == 6) {
+			//do something 
+		}
 		 
 		 System.out.println("\nProgram terminates.");
 		 System.exit(0);
