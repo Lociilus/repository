1139fc9fd5fb483e42022d84baeaea5b8075175b
Lociilus <92417234+Lociilus@users.noreply.github.com>
2021-10-14 12:18:48 -0400

    Update Calculator.java

--- Calculator.java/src/Calculator.java
+++ Calculator.java/src/Calculator.java
@@ -1,34 +1,39 @@
+import java.lang.Math;
+	public class Calculator 
+	{
+		// public void start()
+//		{
+//			
+//		}
+		
+		public double addition(double num1, double num2)
+		{
+			return (num1 + num2);
+		}
+		
+		public double subtraction(double num1, double num2)
+		{
+			return (num1 - num2);
+		}
+		
+		public double multiplication(double num1, double num2)
+		{
+			return (num1 * num2);
+		}
+		public double division(double num1, double num2)
+		{
+			return (num1 / num2);
+		}
+		public double squareroot(double num1, double num2)
+		{
+			return (Math.sqrt(num1));
+		}
+		
+		public void moveToDisplay()
+		{
+			DisplayResults myDisplayResults  = new DisplayResults();
+			
+			//myDisplayResults.start();
+		}
+	}
 
-public class Calculator 
-{
-	// public void start()
-	{
-		
-	}
-	
-	public double addition(double num1, double num2)
-	{
-		return (num1 + num2);
-	}
-	
-	public double subtraction(double num1, double num2)
-	{
-		return (num1 - num2);
-	}
-	
-	public double multiplication(double num1, double num2)
-	{
-		return (num1 * num2);
-	}
-	public double division(double num1, double num2)
-	{
-		return (num1 / num2);
-	}
-	
-	public void moveToDisplay()
-	{
-		DisplayResults myDisplayResults  = new DisplayResults();
-		
-		myDisplayResults.start();
-	}
-}
