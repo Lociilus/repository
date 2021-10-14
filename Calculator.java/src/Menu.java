dbba496ecc69650b928a791a1468c15cbd1a5900
Lociilus <92417234+Lociilus@users.noreply.github.com>
2021-10-14 12:20:26 -0400

    Update Menu.java

--- Calculator.java/src/Menu.java
+++ Calculator.java/src/Menu.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Menu 
 {
 	//public void start() {
-	}
 		private Scanner kbdInput = new Scanner(System.in);
 		private Calculator myCalc = new Calculator();
 		private DisplayResults myDisplay = new DisplayResults();
 		
-		int showMenu()
+		public int showMenu()
 		{
 			System.out.println("Five-Function Calculator");
 			System.out.println("Select a calculation by entering the number to \n" + "the left of the operation.\n");
@@ -21,28 +20,16 @@ public class Menu
 			System.out.println("6.\tQuit");
 			
 			System.out.print("\nPlease enter the number of the desired operation; ");
-			return kbdInput.nextInt();}
+			return kbdInput.nextInt();
+			
+		
+		}
 		
 		
-		public void getUserValue()
-		{
-			double value1 = 0.0;
-			double value2 = 0.0;
+		
 			
-			System.out.print("Please enter a numerical value: ");
-			value1 = kbdInput.nextDouble();
-			
-			System.out.print("Please enter another numerical value: ");
-			value2 = kbdInput.nextDouble();
-			
-			myDisplay.showResult(value1, value2, myCalc.addition(value1, value2));
-			
-			Calculator myCalculator = new Calculator();
-			
-			myCalculator.start();
+			//myCalculator.start();
 		
 		}
 
-	}
-
-
+	
